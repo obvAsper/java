bd102f75e14131558316b4d6021a6960785e9579
obvAsper <114305204+obvAsper@users.noreply.github.com>
2023-05-10 11:11:29 +0200

    Update inv.java

--- src/main/java/de/joeakeem/spigotmc/plugin/template/commands/inv.java
+++ src/main/java/de/joeakeem/spigotmc/plugin/template/commands/inv.java
@@ -1,1 +1,10 @@
+package de.joeakeem.spigotmc.plugin.template.commands;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
